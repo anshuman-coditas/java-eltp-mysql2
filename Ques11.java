5f506e8ee9668cde728f349c47878e8de5dcbe15
Anshuman Kumar <anshuman.kumar@coditas.com>
2022-02-25 15:56:00 +0530

    Initial Commit

--- Ques11.java
+++ Ques11.java
@@ -10,13 +10,15 @@ public class Ques11 {
         Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/studentdemo", "root", "root");
         System.out.println("Connected");
         con.setAutoCommit(false);
-        Statement st=con.createStatement();
-        String ins="insert into student values(11,'Anshuman',4,'Mumbai',99.4,'M')";
-        st.execute(ins);
-        Savepoint sp1=con.setSavepoint();
-        Statement st1=con.createStatement();
-        String ins1="insert into student values(12,'Harvey',4,'Mumbai',99.4,'M')";
-        st.execute(ins1);
+        String ins="update student set name='ak' where id=4";
+            PreparedStatement ps= con.prepareStatement(ins);
+            ps.execute();
+         System.out.println("Updated");
+         Savepoint sp1=con.setSavepoint();
+        /*String ins1="update student set name='ak' where id=7";
+        PreparedStatement ps1= con.prepareStatement(ins1);
+        ps1.execute();
+        System.out.println("Updated");*/
         con.rollback(sp1);
         Statement st2= con.createStatement();
         String show="select * from student";
@@ -37,16 +39,33 @@ public class Ques11 {
 }
 /*
 
-Connected
+CConnected
+Updated
+Updated
+BEFORE ROLLBACK(SAVEPOINT)
 --------------------------------------------------------------------------------------------------------------------------
              ID            NAME            YEAR      PERCENTAGE            CITY          GENDER
 ---------------------------------------------------------------------------------------------------------------------------
-             1      Mr. Mr. Alex              1           Delhi           90.0               M
+             1                ak              1           Delhi           90.0               M
              2      Mr. Mr. Mike              2           Delhi           89.0               M
-             4    Mr. Mr. Harvey              3          Mumbai           96.0               M
+             4                ak              3          Mumbai           96.0               M
+             7                ak              2           Delhi           79.0               M
+             8    Mr. Mr. Arthur              3       Bangalore           95.0               M
+             9          Anshuman              4           Delhi           98.0               M
+            10          Anshuman              3          Mumbai           93.0               M
+
+
+            AFTER ROLLBACK(SAVEPOINT)
+            Connected
+Updated
+--------------------------------------------------------------------------------------------------------------------------
+             ID            NAME            YEAR      PERCENTAGE            CITY          GENDER
+---------------------------------------------------------------------------------------------------------------------------
+             1                ak              1           Delhi           90.0               M
+             2      Mr. Mr. Mike              2           Delhi           89.0               M
+             4                ak              3          Mumbai           96.0               M
              7    Mr. Mr. Thomas              2           Delhi           79.0               M
              8    Mr. Mr. Arthur              3       Bangalore           95.0               M
              9          Anshuman              4           Delhi           98.0               M
             10          Anshuman              3          Mumbai           93.0               M
-            11          Anshuman              4          Mumbai           99.0               M
  */
--- Ques7.java
+++ Ques7.java
@@ -1,10 +1,7 @@
 package MysqlAssignment2;
 
 import javax.swing.plaf.nimbus.State;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.Statement;
+import java.sql.*;
 
 public class Ques7 {
     public static void main(String[] args) {
@@ -13,10 +10,10 @@ public class Ques7 {
             Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/studentdemo", "root", "root");
             System.out.println("Connected");
             con.setAutoCommit(false);
-
-            Statement st=con.createStatement();
-            String ins="insert into student values(11,'Anshuman',4,'Mumbai',99.4,'M')";
-            st.execute(ins);
+            /*String ins="update student set name='ak' where id=4";
+            PreparedStatement ps= con.prepareStatement(ins);
+            ps.execute();*/
+//            System.out.println("Updated");
             con.rollback();
             Statement st1= con.createStatement();
             String show="select * from student";
@@ -29,6 +26,8 @@ public class Ques7 {
                 System.out.printf("%14s %17s %14s %15s %14s %15s ",rs.getInt(1),rs.getString(2),rs.getInt(3),
                         rs.getString(4),rs.getFloat(5),rs.getString(6));
                 System.out.println();
+
+
             }
 
 
@@ -39,14 +38,34 @@ public class Ques7 {
 }
 /*
 Connected
+Updated
+BEFORE ROLLBACK
 --------------------------------------------------------------------------------------------------------------------------
              ID            NAME            YEAR      PERCENTAGE            CITY          GENDER
 ---------------------------------------------------------------------------------------------------------------------------
-             1      Mr. Mr. Alex              1           Delhi           90.0               M
+             1                ak              1           Delhi           90.0               M
+             2      Mr. Mr. Mike              2           Delhi           89.0               M
+             4                ak              3          Mumbai           96.0               M
+             7    Mr. Mr. Thomas              2           Delhi           79.0               M
+             8    Mr. Mr. Arthur              3       Bangalore           95.0               M
+             9          Anshuman              4           Delhi           98.0               M
+            10          Anshuman              3          Mumbai           93.0               M
+
+
+            AFTER ROLLBACK
+            Connected
+--------------------------------------------------------------------------------------------------------------------------
+             ID            NAME            YEAR      PERCENTAGE            CITY          GENDER
+---------------------------------------------------------------------------------------------------------------------------
+             1                ak              1           Delhi           90.0               M
              2      Mr. Mr. Mike              2           Delhi           89.0               M
              4    Mr. Mr. Harvey              3          Mumbai           96.0               M
              7    Mr. Mr. Thomas              2           Delhi           79.0               M
              8    Mr. Mr. Arthur              3       Bangalore           95.0               M
              9          Anshuman              4           Delhi           98.0               M
             10          Anshuman              3          Mumbai           93.0               M
+
+
+
+
  */
